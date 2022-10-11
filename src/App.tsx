e841804e3fae1d91a9544cfadc2a605502410c04
Madox Hussey <mdxblue@gmail.com>
2022-10-11 18:28:20 -0400

    added columns button and list

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,34 @@
 import React from "react";
+import { Button, Col, Container, Row } from "react-bootstrap";
 import "./App.css";
 
 function App(): JSX.Element {
     return (
-        <div className="App">
-            <header className="App-header">
-<<<<<<< HEAD
-                Madox Hussey - Hello World - COS420 - UD CISC275 with React
-                Hooks and TypeScript
-=======
-                UM COS420 with React Hooks and TypeScript
->>>>>>> upstream/task-html-css
-            </header>
+        <div className="App" role="banner">
+            <h1 className="banner">
+                Madox Hussey - Hello World - COS420 with React Hooks and
+                TypeScript with React Hooks and TypeScript
+            </h1>
+            <img
+                src="https://image.shutterstock.com/image-vector/face-emoticon-icon-vector-logo-260nw-1714204030.jpg"
+                alt="A smiley face"
+            />
+            <Container>
+                <Row>
+                    <Col>
+                        <ul>
+                            <li>First item</li>
+                            <li>Second item</li>
+                            <li>Third item</li>
+                        </ul>
+                    </Col>
+                    <Col>
+                        <Button onClick={() => console.log("Hello World!")}>
+                            Log Hello World
+                        </Button>
+                    </Col>
+                </Row>
+            </Container>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
