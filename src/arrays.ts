401c1d0bd47d58921c93898cd708323af010692c
Madox Hussey <mdxblue@gmail.com>
2022-10-22 17:22:49 -0400

    Wrote toInts function

--- src/arrays.ts
+++ src/arrays.ts
@@ -26,7 +26,10 @@ export function tripleNumbers(numbers: number[]): number[] {
  * the number cannot be parsed as an integer, convert it to 0 instead.
  */
 export function stringsToIntegers(numbers: string[]): number[] {
-    return [];
+    const toInts: number[] = numbers.map((str: string): number =>
+        isNaN(parseInt(str)) ? 0 : parseInt(str)
+    );
+    return toInts;
 }
 
 /**
