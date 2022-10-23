bb2d404f922a99d02f9e066aa20d51adff933d5f
Madox Hussey <mdxblue@gmail.com>
2022-10-23 18:13:11 -0400

    wrote toMarkdown

--- src/objects.ts
+++ src/objects.ts
@@ -87,7 +87,11 @@ export function toShortForm(question: Question): string {
  * Check the unit tests for more examples of what this looks like!
  */
 export function toMarkdown(question: Question): string {
-    return "";
+    let str: string = "# " + question.name + "\n" + question.body;
+    if (question.type === "multiple_choice_question") {
+        str = str + "\n- " + question.options.join("\n- ");
+    }
+    return str;
 }
 
 /**
