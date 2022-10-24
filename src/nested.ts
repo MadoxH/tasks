68a1aca9d323bf2f0abe4f2c08c40d89bb640147
Madox Hussey <mdxblue@gmail.com>
2022-10-23 21:39:11 -0400

    wrote sumPoints

--- src/nested.ts
+++ src/nested.ts
@@ -74,7 +74,12 @@ export function getNames(questions: Question[]): string[] {
  * Consumes an array of questions and returns the sum total of all their points added together.
  */
 export function sumPoints(questions: Question[]): number {
-    return 0;
+    const pointsOnly: number[] = questions.map((n) => n.points);
+    const sumOfPoints: number = pointsOnly.reduce(
+        (sum: number, num: number) => sum + num,
+        0
+    );
+    return sumOfPoints;
 }
 
 /***
